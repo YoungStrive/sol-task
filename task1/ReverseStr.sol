b546ae3748437737c3f8c41b4dab3f9feb6fd0aa
Lambda <60801363+YoungStrive@users.noreply.github.com>
2025-09-25 21:52:15 +0800

    Update ReverseStr.sol

--- task1/ReverseStr.sol
+++ task1/ReverseStr.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+contract  task1_2{
+
+  
+
+    function  getReverse(string memory str)  public pure  returns (string memory){
+
+        bytes memory bs = bytes(str);
+        uint   bsSize = bs.length;
+        bytes  memory reverseBys = new bytes(bsSize);
+        uint j = 0; 
+        for(uint i = bsSize; i>0; i--){
+            reverseBys[j] = bs[i-1];
+            j++;
+        }
+        return string(reverseBys);
+
+   }
+
+
+}
