1459e860c991f2308046a0c87842a7915f4ff91b
Lambda <60801363+YoungStrive@users.noreply.github.com>
2025-09-25 21:59:31 +0800

    Create RomeToNum.sol

---
+++ task1/RomeToNum.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Task1_3{
+
+     //定义一个映射
+     mapping(bytes32 rome => uint16 num)  private  romeNumMap;
+     //构造函数初始化映射
+     constructor() {
+        romeNumMap["I"]=1;
+        romeNumMap["V"]=5;
+        romeNumMap["X"]=10;
+        romeNumMap["L"]=50;
+        romeNumMap["C"]=100;
+        romeNumMap["D"]=500;
+        romeNumMap["M"]=1000;
+      }
+
+   function  ChangeRomeNum( string memory _input) public  view  returns (uint){
+      // 将string 转 bytes
+      bytes memory romeBytes =  bytes(_input);
+      uint size = romeBytes.length;
+      uint sum = 0;
+      for(uint i = 0; i<size;){
+            // 获取当前字符的数值
+            uint current = romeNumMap[romeBytes[i]];
+            // 检查下一个字符是否存在且构成减法规则
+            if (i + 1 < size) {
+                uint next = romeNumMap[romeBytes[i + 1]];
+                if (current < next) {
+                    sum += (next - current);
+                    i += 2; 
+                    continue;
+                }
+            } 
+              
+            // 普通加法规则
+            sum += current;
+            i += 1;
+         }
+      
+         return sum;
+
+   
+   }
+}
