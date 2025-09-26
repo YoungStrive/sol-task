dbf1ca784b99574ffe09f48a7725fe1f2f2f816f
Lambda <60801363+YoungStrive@users.noreply.github.com>
2025-09-26 22:38:09 +0800

    Update NumToRome.sol

--- task1/NumToRome.sol
+++ task1/NumToRome.sol
@@ -4,105 +4,85 @@ pragma solidity ^0.8.26;
 
 contract task1_4{
 
-         string  [] strArray;
          //位置的map
-         mapping(string localStr=>string romeStr) refMap;
-    function changeNum(uint _num) public  returns (string memory result){
+      mapping(string localStr=>string romeStr) refMap;
+      constructor() {
+        refMap["G"]="I";
+        refMap["G4"]="IV";
+        refMap["G5"]="V";
+        refMap["G9"]="IX";
+        refMap["S"]="X";
+        refMap["S4"]="XL";
+        refMap["S5"]="L";
+        refMap["S9"]="XC";
+        refMap["B"]="C";
+        refMap["B4"]="C";
+        refMap["B5"]="D";
+        refMap["B9"]="CM";
+      }
+   
+   string []  strArray;
+    function changeNum(uint _num) public  returns(string memory){
         uint q= _num/1000;
         uint b= (_num%1000)/100;
         uint s= (_num%100)/10;
         uint g= (_num%10);
         //千位有值
-        if(q!=0){
+        if(q!=0 ){
            for(uint i=0; i<q; i++){
                 strArray.push("M");
            }
         }
-        //百
-        if(b>=5){
-             if(b==9){
-                strArray.push("MD");
-             }else{ 
-                strArray.push("D");
-                for(uint i=0;i<b-5;i++ ){
-                    strArray.push("C");
-                }
-             }
-        }else{
-            if(b==4){
-                strArray.push("DC");
-            }else{
-                for(uint i=0;i<b;i++){
-                    strArray.push("c");
-                }
-            }
+        if(b!=0){
+            handl(b,"B");
+        }
+        if(s!=0){
+            handl(s,"S");
+        }
+        if(g!=0){
+            handl(g,"G");
+        }
+        string memory resultStr;
+        for(uint i=0;i<strArray.length; i++){
+            resultStr = strConcat(resultStr,strArray[i]);
+        }
+         //清空数组
+        while (strArray.length==0){
+            strArray.pop();
         }
 
-        //十
-       if(s>=5){
-            if(s==9){
-                strArray.push("CX");
-             }else{ 
-                strArray.push("L");
-                for(uint i=0;i<s-5;i++ ){
-                    strArray.push("X");
-                }
-             }
-       }else{
-         if(s==4){
-                strArray.push("LX");
-            }else{
-                for(uint i=0;i<s;i++){
-                    strArray.push("X");
-                }
-            }
-       }
-
-       //个
-
-    if(g>=5){
-         if(g==9){
-                strArray.push("XI");
-             }else{ 
-                strArray.push("V");
-                for(uint i=0;i<g-5;i++ ){
-                    strArray.push("I");
-                }
-             }
-             
-        }else{     
-        if(g==4){
-                strArray.push("VI");
-            }else{
-                for(uint i=0;i<g;i++){
-                    strArray.push("I");
-                }
-            }
-       }
-       return strArray[0];
+      
+      return resultStr;
     }
 
 
 
     // 处理数据
-    function handl(uint num,string memory statusFlag, string[]  memory strArray) private {
-        refMap["G"]="I";
-        refMap["G4"]="VI";
-        refMap["G5"]="V";
-        refMap["G9"]="XI";
-        refMap["S"]="X";
-        refMap["S4"]="LX";
-        refMap["S5"]="L";
-        refMap["S9"]="CX";
-        refMap["B"]="C";
-        refMap["B4"]="DC";
-        refMap["B5"]="D";
-        refMap["B9"]="MB";
+    function handl(uint num,string memory statusFlag) private {
+   
          //大于等于5
          if(num>=5){
+            //等于9 获取特殊的
              if(num==9){
-                 string memory strFlag = strConcat(statusFlag,"9");  
+                 string memory strFlag = strConcat(statusFlag,"9");
+                 strArray.push(refMap[strFlag]);
+             }else{
+               string memory strFlag5 = strConcat(statusFlag,"5");
+               strArray.push(refMap[strFlag5]);
+               for(uint  i=0;i<num-5;i++){
+                  strArray.push(refMap[statusFlag]);
+               }
              }
+         }else{
+           //那就是小于五的
+           if(num==4){
+               string memory strFlag4 = strConcat(statusFlag,"4");
+               strArray.push(refMap[strFlag4]);
+           }else{
+                for(uint i=0; i<num; i++){
+                    strArray.push(refMap[statusFlag]);
+                }
+           }
 
          }
 
